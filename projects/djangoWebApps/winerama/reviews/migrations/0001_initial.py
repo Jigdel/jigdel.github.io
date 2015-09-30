4047f398ce22d87c927a9da65f94a9e9c0c5da72
Jigdel <jigdel.k@gmail.com>
2015-09-29 20:39:01 -0400

    Wine recommendation web app from codementor.io

--- projects/djangoWebApps/winerama/reviews/migrations/0001_initial.py
+++ projects/djangoWebApps/winerama/reviews/migrations/0001_initial.py
@@ -13,17 +13,17 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='Review',
             fields=[
-                ('id', models.AutoField(serialize=False, auto_created=True, verbose_name='ID', primary_key=True)),
-                ('pub_date', models.DateTimeField(verbose_name='date published')),
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('pub_date', models.DateTimeField(verbose_name=b'date published')),
                 ('user_name', models.CharField(max_length=100)),
                 ('comment', models.CharField(max_length=200)),
-                ('rating', models.IntegerField(choices=[(1, '1'), (2, '2'), (3, '3'), (4, '4'), (5, '5')])),
+                ('rating', models.IntegerField(choices=[(1, b'1'), (2, b'2'), (3, b'3'), (4, b'4'), (5, b'5')])),
             ],
         ),
         migrations.CreateModel(
             name='Wine',
             fields=[
-                ('id', models.AutoField(serialize=False, auto_created=True, verbose_name='ID', primary_key=True)),
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
                 ('name', models.CharField(max_length=200)),
             ],
         ),
--- projects/djangoWebApps/winerama/reviews/urls.py
+++ projects/djangoWebApps/winerama/reviews/urls.py
@@ -1,20 +1,5 @@
-"""reviews URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/1.8/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
-Including another URLconf
-    1. Add an import:  from blog import urls as blog_urls
-    2. Add a URL to urlpatterns:  url(r'^blog/', include(blog_urls))
-"""
-
 from django.conf.urls import url
+
 from . import views
 
 urlpatterns = [
@@ -27,6 +12,10 @@ urlpatterns = [
     # ex: /wine/5/
     url(r'^wine/(?P<wine_id>[0-9]+)/$', views.wine_detail, name='wine_detail'),
     url(r'^wine/(?P<wine_id>[0-9]+)/add_review/$', views.add_review, name='add_review'),
+    # ex: /review/user - get reviews for the logged user
+    url(r'^review/user/(?P<username>\w+)/$', views.user_review_list, name='user_review_list'),
+    # ex: /review/user - get reviews for the user passed in the url
+    url(r'^review/user/$', views.user_review_list, name='user_review_list'),
+    # ex: /recommendation - get wine recommendations for the logged user
+    url(r'^recommendation/$', views.user_recommendation_list, name='user_recommendation_list'),
 ]
-
-
